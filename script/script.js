4e32b3ceb2351a111fc5285cf1ed10631860d1e1
guienjoris <49984305+guienjoris@users.noreply.github.com>
2019-07-26 11:27:30 +0200

    update toggle fini

--- script/script.js
+++ script/script.js
@@ -1,8 +1,18 @@
+
 var div_contact = document.getElementById('div_contact');
 var menu = document.getElementById('menu');
 var burgerbar= document.getElementsByClassName('burger');
 var nav= document.getElementById('nav');
-
+var toggle =document.getElementById('toggle');
+var border_contact= document.getElementsByClassName('contact_border');
+var ion_icon = document.getElementsByTagName('ion-icon');
+var domaines = document.getElementById('domaines');
+var presentation = document.getElementById('presentation')
+window.onload = ()=>{
+    if(localStorage.getItem("modenuit")==="nuit"){
+        toggle_f();
+    }
+}
 function display(){
     if(div_contact.style.display === "none"){
     div_contact.style.display = "flex";
@@ -13,8 +23,8 @@ function display(){
 }
 menu.addEventListener("click", ()=>{ 
     if(nav.style.display ==="none"){
-        menu.style.width ="90vw";
-        menu.style.height= "10vh";
+        menu.style.width ="96vw";
+        menu.style.height= "7vh";
         burgerbar[0].style.transform="rotate(45deg)";
         burgerbar[0].style.width = "20px"
         burgerbar[1].style.display="none";
@@ -33,3 +43,33 @@ menu.addEventListener("click", ()=>{
     nav.style.display ="none";    
 }
 });
+
+function toggle_f(){
+    var toggleAttribute= toggle.getAttribute("name") ;
+    console.log(toggleAttribute);
+    if(toggleAttribute === "moon"){
+        toggle.setAttribute("name", "sunny");
+        border_contact[0].style.backgroundColor = "black";
+        ion_icon[1].style.color = "white";
+        ion_icon[2].style.color = "white";
+        ion_icon[3].style.color = "white";
+        ion_icon[4].style.color = "white";
+        domaines.style.color="white";
+        document.body.style.backgroundColor="#242C37";
+        presentation.style.color = "white";
+        localStorage.setItem('modenuit', 'nuit');
+    }
+    if(toggleAttribute === "sunny"){
+        toggle.setAttribute("name","moon");
+        border_contact[0].style.backgroundColor = "white";
+        ion_icon[1].style.color = "black";
+        ion_icon[2].style.color = "black";
+        ion_icon[3].style.color = "black";
+        ion_icon[4].style.color = "black";
+        domaines.style.color="black";
+        presentation.style.color = "black";
+        document.body.style.backgroundColor="white";
+        localStorage.setItem('modenuit', 'jour');
+
+    }
+}
